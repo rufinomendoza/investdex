7834a0a76d9a4147c081e6323405a737fdeab9b2
Rufino Mendoza <rlmendoza@aol.com>
2014-06-26 12:14:49 -0400

    Add draft Bookmarklet

---
+++ app/assets/javascripts/bookmarklet.js
@@ -0,0 +1,10 @@
+// Currently not functional
+// function (){
+//   var title = encodeURIComponent(document.title);
+//   var url = encodeURIComponent(document.location);
+//   m = 'http://localhost:3000/entries/new/';
+//   w = window.open(m,'addwindow','status=no,toolbar=no,width=1024,height=768,resizable=yes');
+//   setTimeout(function(){w.focus();}, 250);
+//   document.getElementById('entry_title').value = title;
+//   document.getElementById('entry_url').value = url;
+// }
